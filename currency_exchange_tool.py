240c9ffffbde6ca373f744bcb2d600945394f6a3
MrRufusWalkden <rufus.walkden@LAETottenham.ac.uk>
2024-09-30 10:05:00 +0100

    Create currency_exchange_tool.py

---
+++ currency_exchange_tool.py
@@ -0,0 +1,13 @@
+import math # you'll probably need this
+
+exchange_rates = {
+    'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
+    'EUR': 1.15,
+}
+
+def check_currency_exists(currency):
+    return
+
+def currency_convert(original_c, new_c, amount):
+    # your code here
+    return
