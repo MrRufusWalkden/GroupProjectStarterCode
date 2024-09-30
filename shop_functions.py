3b09c67422bf231727d51104290e0da9d5b73769
MrRufusWalkden <rufus.walkden@LAETottenham.ac.uk>
2024-09-30 10:04:08 +0100

    Create shop_functions.py

---
+++ shop_functions.py
@@ -0,0 +1,19 @@
+items = {
+    'Bread': 1.20,
+    'Milk': 1.15,
+    'Chocoloate': 0.5,
+}
+
+def start_shop():
+    while True:
+        print("What you would like to buy? ")
+        # display items to buy
+
+    # blah blah blah
+
+    return {
+        'items': [],
+        'total_cost': 0
+    }
+        
+        
