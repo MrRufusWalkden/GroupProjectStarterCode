d5e338ac4b03e6c6654ad8697d3807727c0a5524
MrRufusWalkden <rufus.walkden@LAETottenham.ac.uk>
2024-09-30 10:03:33 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from currency_exchange_tool import currency_convert
+from shop_functions import start_shop
+
+print('Welcome to my shop')
+
+while True:
+    print('Please select what you would like to buy')
+    items_to_buy = start_shop()
+
+    # blah blah 
